2b4cd4532d6d64acaf5564a2c58772d358e8c397
Matt Gabor <mattgabor@gmail.com>
2017-02-04 20:27:24 -0800

    removed node_modules ignore

--- p1/js/viz.js
+++ p1/js/viz.js
@@ -29,28 +29,31 @@ var svg = d3.select("#scatter").append("svg")
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// load csv data
-d3.csv("allschoolsfiltered.csv", function(error, data) {
-  if (error) throw error;
+loadData("US");
 
-  // convert strings in csv to integers
-  data.forEach(function(d) {
-    d.cost = +d.adm_rate;
-    d.earnings = +d.earnings;
-  });
+function loadData(dfilter) {
+  // load csv data
+  d3.csv("filteredschools.csv", function(error, data) {
+    if (error) throw error;
 
-  // use maximum and minimum values of data for x and y domain
-  x.domain(d3.extent(data, function(d) { return d.adm_rate; })).nice();
-  y.domain(d3.extent(data, function(d) { return d.earnings; })).nice();
+    // convert strings in csv to integers
+    data.forEach(function(d) {
+      d.cost = +d.adm_rate;
+      d.earnings = +d.earnings;
+    });
 
-  // append x axis to svg group
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
+    // use maximum and minimum values of data for x and y domain
+    x.domain(d3.extent(data, function(d) { return d.adm_rate; })).nice();
+    y.domain(d3.extent(data, function(d) { return d.earnings; })).nice();
 
-  // text label for y axis
-  svg.append("text")
+    // append x axis to svg group
+    svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis)
+
+    // text label for y axis
+    svg.append("text")
     .attr("transform", "rotate(-90)")
     .attr("x", 0 - (height/2))
     .attr("y", 0 - margin.left + 10)
@@ -59,8 +62,8 @@ d3.csv("allschoolsfiltered.csv", function(error, data) {
     .style("text-anchor", "middle")
     .text("Median Earnings 10 years after graduating ($)")
 
-  // text label for x axis
-  svg.append("text")
+    // text label for x axis
+    svg.append("text")
     .attr("transform", "translate("+ (width/2) +","+ height +")")
     .attr("y", margin.bottom - 20)
     // .attr("dx", "1em")
@@ -68,41 +71,108 @@ d3.csv("allschoolsfiltered.csv", function(error, data) {
     .style("text-anchor", "middle")
     .text("Acceptance Rate");
 
-  // append y axis to svg group
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
+    // append y axis to svg group
+    svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis)
 
-  // create points
-  svg.selectAll(".dot")
-      .data(data)
+    // create tooltip
+    var tooltip = d3.tip()
+    .attr("class", "d3-tip")
+    .offset([-10, 0])
+    .html(function(d) {
+      return "School Name: &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<span style='color:#3498db'>"+ d.name + "</span><br>Acceptance Rate: <span style='color:#e74c3c'>" + d.adm_rate + "</span><br>Median Earnings:  <span style='color:#2ecc71'>" + d.earnings + "</span>"
+    })
+
+    svg.call(tooltip);
+
+    // Color scheme
+    var colorScale = d3.scaleOrdinal(d3.schemeCategory20)
+      // .domain([1,2,3,6,7,8,9,10]);
+
+    // create points
+    svg.selectAll(".dot")
+    .data(data)
     .enter().append("circle")
-      .attr("class", "dot")
-      // radius
-      .attr("r", 10)
-      .attr("r", function(d) { return  d.pop/2000; })
-      // x and y axis coords of the center of the element
-      .attr("cx", function(d) { return x(d.adm_rate); })
-      .attr("cy", function(d) { return y(d.earnings); })
-      .style("fill", function(d) { return color(d.name); });
+    .filter(function(d) { return d.state == "CA" || d.state == "TX" || d.state == "FL" || d.state == "NY" || d.state == "IL"})
+    .attr("class", "dot")
+    // radius
+    .attr("r", 10)
+    .attr("r", function(d) { return  d.pop/2000; })
+    // x and y axis coords of the center of the element
+    .attr("cx", function(d) { return x(d.adm_rate); })
+    .attr("cy", function(d) { return y(d.earnings); })
+    .style("fill", function(d) { return colorScale(d.state); })
+    .on('mouseover', tooltip.show)
+    .on('mouseout', tooltip.hide);
 
-  // var legend = svg.selectAll(".legend")
-  //     .data(color.domain())
-  //   .enter().append("g")
-  //     .attr("class", "legend")
-  //     .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
-  //
-  // legend.append("rect")
-  //     .attr("x", width - 18)
-  //     .attr("width", 18)
-  //     .attr("height", 18)
-  //     .style("fill", color);
-  //
-  // legend.append("text")
-  //     .attr("x", width - 24)
-  //     .attr("y", 9)
-  //     .attr("dy", ".35em")
-  //     .style("text-anchor", "end")
-  //     .text(function(d) { return d; });
 
-});
+    var legend = svg.selectAll(".legend")
+    .data(colorScale.domain())
+    .enter().append("g")
+    .attr("class", "legend")
+    .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+    legend.append("rect")
+    .attr("x", width - 18)
+    .attr("width", 18)
+    .attr("height", 18)
+    .style("fill", colorScale);
+
+    legend.append("text")
+    .attr("x", width - 24)
+    .attr("y", 9)
+    .attr("dy", ".35em")
+    .style("text-anchor", "end")
+    .text(function(d) { return d; });
+
+  });
+}
+
+
+
+// // Update data section (Called from the onclick)
+// function filterData(selection) {
+//     // Get the data again
+//     d3.csv("filteredschools.csv", function(error, data) {
+//         if (error) throw error;
+//
+//         // convert strings in csv to integers
+//         data.forEach(function(d) {
+//           d.cost = +d.adm_rate;
+//           d.earnings = +d.earnings;
+//         });
+//
+//
+//         // use maximum and minimum values of data for x and y domain
+//         x.domain(d3.extent(data, function(d) { return d.adm_rate; })).nice();
+//         y.domain(d3.extent(data, function(d) { return d.earnings; })).nice();
+//
+//     // Select the section we want to apply our changes to
+//     var svg = d3.select("#scatter").transition();
+//
+//     svg.selectAll(".dot")
+//       .attr("r", 20);
+//     // Make the changes
+//
+//
+//         //     .duration(750)
+//         //     .attr("d", valueline(data));
+//         // svg.select(".x.axis") // change the x axis
+//         //     .duration(750)
+//         //     .call(xAxis);
+//         // svg.select(".y.axis") // change the y axis
+//         //     .duration(750)
+//         //     .call(yAxis);
+//
+//     });
+// // }
+//
+//
+//     if (dfilter == "US") {
+//       console.log("US");
+//     } else if (dfilter == 'CA') {
+//       data.filter(function(d) { return d.state == "CA" });
+//     } else if (dfilter == "top5") {
+//       data.filter(function(d) { return d.state == "CA" || d.state == "TX" || d.state == "FL" || d.state == "NY" || d.state == "IL"});
+//     }
