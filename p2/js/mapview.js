5386cf4ac229f4c99fd3050a0a57cf0573a9ad30
Matt Gabor <mattgabor@gmail.com>
2017-02-14 09:47:08 -0800

    got markers to show up on map and zoom properly

--- p2/js/mapview.js
+++ p2/js/mapview.js
@@ -7,69 +7,75 @@ function buildMap(csvFile, jsonFile) {
     maxZoom: 18,
   }).addTo(map);
 
-  // append the svg to the overlay pane, not the div
-  var svg = d3.select(map.getPanes().overlayPane).append("svg");
+  map._initPathRoot();
 
+  // append the svg to the overlay pane, not the div
+  // var svg = d3.select(map.getPanes().overlayPane).append("svg");
+
+  var svg = d3.select("#mapView").select("svg");
   var g = svg.append("g")
     .attr("class", "leaflet-zoom-hide")
 
   d3.json(jsonFile, function(error, mapData) {
     if (error) throw error;
 
-    // convert GeoJSON to SVG
-    var transform = d3.geoTransform({point: projectPoint}),
-        path = d3.geoPath().projection(transform);
+    var count = 0;
+
+    // coordinates are backwards
+    mapData.features.forEach(function(d) {
+      d.LatLng = new L.LatLng(d.geometry.coordinates[1], d.geometry.coordinates[0])
+      count += 1;
+    })
+
+    console.log(count)
 
     // create circles for each feature
-    var feature = svg.selectAll(".marker")
+    var feature = g.selectAll(".marker")
       .data(mapData.features)
-      .enter().append("circle", ".marker")
-      .attr("r", 5)
+      .enter().append("circle")
+      .style("stroke", "black")
+      .style("fill", "#0070CB")
+      .attr("r", 8)
       .attr("class", "marker")
-      // .attr("cx", function (d) { return })
+      .on("click", function (d) {
+        console.log(d.properties.start_station)
+      })
+      .on("mouseover", function(d) {d3.select(this).style("cursor", "pointer")})
 
 
-    // map.on("viewreset", updateMap);
-    // updateMap();
+    map.on("viewreset", updateMap);
+    updateMap();
 
-    // init path data
-    feature.attr("d", path);
-
-    // // size svg object
-    // var bounds = path.bounds(data),
-    //     topLeft = bounds[0],
-    //     bottomRight = bounds[1];
-    // //
-    svg.attr("width", 800)
-       .attr("height", 600);
-    // set dimensions of svg & g
-    // svg.attr("width", bottomRight[0] - topLeft[0])
-    //   .attr("height", bottomRight[1] - topLeft[1])
-    //   .style("left", topLeft[0] + "px")
-    //   .style("top", topLeft[1] + "px");
-
-    // g.attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
+    function updateMap() {
+ 		   feature.attr("transform",
+ 		   function(d) {
+   	     return "translate("+
+   		    map.latLngToLayerPoint(d.LatLng).x +","+
+   				map.latLngToLayerPoint(d.LatLng).y +")";
+   			}
+ 		)
+ 	}
 
     // update position of data on map when resized
-    function updateMap() {
-      bounds = path.bounds(mapData);
-
-      var topLeft = bounds[0],
-        bottomRight = bounds[1];
-
-      svg .attr("width", bottomRight[0] - topLeft[0])
-        .attr("height", bottomRight[1] - topLeft[1])
-        .style("left", topLeft[0] + "px")
-        .style("top", topLeft[1] + "px");
-
-      g.attr("transform", "translate(" + -topLeft[0] + ","
-                                      + -topLeft[1] + ")");
-
-      // initialize the path data
-      feature.attr("d", path)
-        .style("fill-opacity", 0.7)
-        .attr('fill','blue');
-		}
+    // function updateMap() {
+    //   var bounds = path.bounds(mapData);
+    //
+    //   var topLeft = bounds[0],
+    //     bottomRight = bounds[1];
+    //
+    //   svg.attr("width", bottomRight[0] - topLeft[0])
+    //     .attr("height", bottomRight[1] - topLeft[1])
+    //     .style("left", topLeft[0] + "px")
+    //     .style("top", topLeft[1] + "px");
+    //
+    //   g.attr("transform", "translate(" + -topLeft[0] + ","
+    //                                   + -topLeft[1] + ")");
+    //
+    //   // initialize the path data
+    //   feature.attr("d", path)
+    //     .style("fill-opacity", 0.7)
+    //     .attr('fill','blue');
+		// }
     // transform coordinates from leaflet's coords to d3's
     function projectPoint(x, y) {
       var point = map.latLngToLayerPoint(new L.LatLng(y, x))
