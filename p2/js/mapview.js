082338c583ca285098d0c8fa3ee805b1d60c15bb
Matt Gabor <mattgabor@gmail.com>
2017-02-15 21:30:58 -0800

    hist with sample data working

--- p2/js/mapview.js
+++ p2/js/mapview.js
@@ -10,17 +10,16 @@ function buildMap(csvFile, jsonFile) {
 
   map._initPathRoot();
 
-  // append the svg to the overlay pane, not the div
-  // var svg = d3.select(map.getPanes().overlayPane).append("svg");
-
-  var svg = d3.select("#mapView")
+  var mapSvg = d3.select("#mapView")
     .select("svg")
 
-  var g = svg.append("g")
+  var g = mapSvg.append("g")
     .attr("class", "leaflet-zoom-hide");
     // d3.select("#overlay")
     //   .transition()
     //   .style("display", "inline")
+
+
 
   d3.json(jsonFile, function(error, mapData) {
     if (error) throw error;
@@ -30,34 +29,55 @@ function buildMap(csvFile, jsonFile) {
       d.LatLng = new L.LatLng(d.geometry.coordinates[1], d.geometry.coordinates[0])
     })
 
+    // create tooltip
+    var tooltip = d3.tip()
+      .attr("class", "d3-tip")
+      .direction('ne')
+      .offset([0, -5])
+      .html(function(d, i) {
+        return "Station Name: "
+      })
+
+    mapSvg.call(tooltip);
+
+    var selectedStation = 0;
 
     // create circles for each feature
     var feature = g.selectAll(".marker")
       .data(mapData.features)
       .enter().append("circle")
-      .style("stroke", "black")
-      .style("fill", "#0070CB")
       .attr("r", 12)
       .attr("class", "marker")
       .on("click", function (d) {
+        selectedStation = d.properties.start_term;
+        d3.select(this)
+          .style("fill", "#f03b20");
         showOverlay(d);
       })
       .on("mouseover", function(d) {
         d3.select(this)
           .style("cursor", "pointer")
-          .style("fill", "red");
+          .style("fill", "#f03b20");
+        tooltip.show;
       })
       .on("mouseout", function(d) {
-        d3.select(this)
-          .style("fill", "#0070CB");
+        if (d.properties.start_term == selectedStation) {
+          // clear all other selections
+          g.selectAll(".marker")
+            .style("fill-opacity", "0.15");
+          d3.select(this)
+            .style("fill", "#f03b20");
+        } else {
+          d3.select(this)
+            .style("fill", "#31a354");
+        }
+        tooltip.hide;
       })
-
-
 
     map.on("viewreset", updateMap);
     updateMap();
 
-    drawHistogram();
+    drawHistogram(mapData.features);
 
     function updateMap() {
  		   feature.attr("transform",
@@ -80,47 +100,5 @@ function buildMap(csvFile, jsonFile) {
           return selectedPoint.properties.start_station;
         })
     }
-
-    function drawHistogram() {
-
-      var data = d3.range(1000).map(d3.randomBates(10));
-
-      var histSvg = d3.select("#histogram")
-          margin = {top: 10, right: 10, bottom: 10, left: 10},
-          width = +histSvg.attr("width") - margin.left - margin.right,
-          height = +histSvg.attr("height") - margin.top - margin.bottom,
-          g = histSvg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-      var xScale = d3.scaleLinear()
-        .rangeRound([0, width])
-
-      // create bins for times of day
-      var bins = d3.histogram()
-        .domain(xScale.domain())
-        .thresholds(xScale.ticks(20))
-        (data);
-
-      // use bins to set y scale
-      var yScale = d3.scaleLinear()
-        .domain([0, d3.max(bins, function(d) { return d.length; })])
-        .range([height, 0])
-
-      var bar = g.selectAll(".bar")
-        .data(bins)
-        .enter().append("g")
-          .attr("class", "bar")
-          .attr("transform", function(d) { return "translate(" + xScale(d.x0) + "," + yScale(d.length) + ")"; });
-
-      bar.append("rect")
-        .attr("x", 1)
-        .attr("width", xScale(bins[0].x1) - xScale(bins[0].x0) - 1)
-        .attr("height", function(d) { return height - yScale(d.length); });
-
-      // x axis
-      g.append("g")
-          .attr("class", "axis axis--x")
-          .attr("transform", "translate(0," + height + ")")
-          .call(d3.axisBottom(xScale));
-    } // end drawHistogram
   }) // end d3.json
 } // end buildMap
