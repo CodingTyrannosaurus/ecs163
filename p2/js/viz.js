ccac858fa1899465400b18fee29c52b4d472a2dc
Matt Gabor <mattgabor@gmail.com>
2017-02-19 21:11:23 -0800

    viz not working

--- p2/js/viz.js
+++ p2/js/viz.js
@@ -3,4 +3,4 @@ var colorScale20 = d3.scaleOrdinal(d3.schemeCategory20);
 var colorScale10 = d3.scaleOrdinal(d3.schemeCategory10);
 
 // initial d3 station setup
-updateMap("data/startTerm.json", [])
+updateMap("data/stationData.json", [])
